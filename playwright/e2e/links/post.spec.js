cea6b43ed80f150486dcb8c1447edf95581ad11d
Danilo Melin <dmelin88@gmail.com>
2025-10-10 17:12:42 -0300

    :sparkles: feat(links): adiciona novos cenários de teste e refatora o script

--- playwright/e2e/links/post.spec.js
+++ playwright/e2e/links/post.spec.js
@@ -8,15 +8,19 @@ import { getUserWithLink } from '../../support/factories/user.js';
 
 test.describe('Post /api/links', () => {
 
-    test('deve encurtar uma URL quando enviada via body', async({ request }) => {
+    const user =  getUserWithLink();
+    let auth;
+    let link;
+    let token;
 
-        const auth = authService(request);
-        const link = linksService(request);
-
-        const user = getUserWithLink();
+    test.beforeEach(async({ request }) => {
+        auth = authService(request);
+        link = linksService(request);
         await auth.createUser(user);
+        token = await auth.getToken(user);
+    });
 
-        const token = await auth.getToken(user);
+    test('deve encurtar uma URL quando enviada via body', async() => {
         const response = await link.createLink(user.link, token);
 
         expect(response.status()).toBe(201);
@@ -28,6 +32,33 @@ test.describe('Post /api/links', () => {
         expect(data.short_code).toMatch(/^[a-zA-Z0-9]{5}$/);
         expect(data).toHaveProperty('title', user.link.title);
         expect(message).toBe('Link criado com sucesso');
+    });
+
+    test('Deve retornar erro 400 quando a url não for enviada', async() => {
+        const response = await link.createLink({}, token);
+
+        expect(response.status()).toBe(400);
+
+        const { message } = await response.json();
+        expect(message).toBe('O campo \'OriginalURL\' é obrigatório');
+    });
+
+    test('Deve retornar erro 400 quando o título não for enviado', async() => {
+        const response = await link.createLink({ original_url: user.link.original_url }, token);
+
+        expect(response.status()).toBe(400);
+
+        const { message } = await response.json();
+        expect(message).toBe('O campo \'Title\' é obrigatório');
+    });
+
+    test('Deve retornar erro 400 quando a url for inválida', async() => {
+        const response = await link.createLink({ original_url: 'teste@gmail.com', title: user.link.title }, token);
+
+        expect(response.status()).toBe(400);
+
+        const { message } = await response.json();
+        expect(message).toBe('O campo \'OriginalURL\' deve ser uma URL válida');
     });
 
 });
