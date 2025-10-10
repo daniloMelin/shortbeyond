7198f85b074ce7b54fe207db7d833fc33b5d01c7
Danilo Melin <dmelin88@gmail.com>
2025-10-09 22:34:31 -0300

    :sparkles: feat(auth): adiciona testes de login e refatora serviços de autenticação

---
+++ playwright/e2e/auth/login.spec.js
@@ -0,0 +1,85 @@
+import { test, expect } from '@playwright/test';
+
+import { authService } from '../../support/services/auth.js';
+
+import { getUser } from '../../support/factories/user';
+
+
+test.describe('POST /auth/login', () => {
+
+    let auth;
+
+    test.beforeEach(({ request }) => {
+        auth = authService(request);
+    });
+
+    test('Deve realizar o login com sucesso quando as credenciais são válidas', async () => {
+        const user = getUser();
+
+        const respCreate = await auth.createUser(user);
+        expect(respCreate.status()).toBe(201);
+
+        const response = await auth.login(user);
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+
+        expect(body).toHaveProperty('message', 'Login realizado com sucesso');
+        expect(body.data).toHaveProperty('token');
+        expect(body.data.user).toHaveProperty('id');
+        expect(body.data.user).toHaveProperty('name', user.name);
+        expect(body.data.user).toHaveProperty('email', user.email);
+        expect(body.data.user).not.toHaveProperty('password');
+    });
+
+    test('Deve retornar erro 401 quando a senha é inválida', async () => {
+        const user = getUser();
+
+        const respCreate = await auth.createUser(user);
+        expect(respCreate.status()).toBe(201);
+
+        user.password = 'senhaerrada';
+
+        const response = await auth.login(user);
+        expect(response.status()).toBe(401);
+
+        const body = await response.json();
+        expect(body).toHaveProperty('message', 'Credenciais inválidas');
+    });
+
+    test('Deve retornar erro 401 quando o email não esta cadastrado', async () => {
+        const user = getUser();
+
+        user.email = 'emailerrado@example.com';
+
+        const response = await auth.login(user);
+        expect(response.status()).toBe(401);
+
+        const body = await response.json();
+        expect(body).toHaveProperty('message', 'Credenciais inválidas');
+    });
+
+    test('Deve retornar erro 400 quando o email não é informado', async () => {
+        const user = {
+            password: 'senha123'
+        };
+
+        const response = await auth.login(user);
+        expect(response.status()).toBe(400);
+
+        const body = await response.json();
+        expect(body).toHaveProperty('message', 'O campo \'Email\' é obrigatório');
+    });
+
+    test('Deve retornar erro 400 quando a senha não é informada', async () => {
+        const user = {
+            email: 'email@example.com'
+        };
+
+        const response = await auth.login(user);
+        expect(response.status()).toBe(400);
+
+        const body = await response.json();
+        expect(body).toHaveProperty('message', 'O campo \'Password\' é obrigatório');
+    });
+});
--- playwright/e2e/auth/register.spec.js
+++ playwright/e2e/auth/register.spec.js
@@ -2,20 +2,20 @@ import { expect, test } from '@playwright/test';
 
 import { getUser } from '../../support/factories/user';
 
-import { registerService } from '../../support/services/register';
+import { authService } from '../../support/services/auth.js';
 
 test.describe('POST /auth/register', () => {
 
-    let register;
+    let auth;
 
     test.beforeEach(({ request }) => {
-        register = registerService(request);
+        auth = authService(request);
     });
 
-    test('Deve registrar um novo usuário com sucesso', async () => {
+    test('Deve cadastrar um novo usuário com sucesso quando os dados são válidos', async () => {
         const user = getUser();
 
-        const response = await register.createUser(user);
+        const response = await auth.createUser(user);
         expect(response.status()).toBe(201);
 
         const body = await response.json();
@@ -27,66 +27,66 @@ test.describe('POST /auth/register', () => {
         expect(body.user).not.toHaveProperty('password');
     });
 
-    test('Não deve registrar um novo usuário com email já existente', async () => {
+    test('Deve retornar erro 400 quando o email já está em uso', async () => {
         const user = getUser();
 
-        const preCondition = await register.createUser(user);
+        const preCondition = await auth.createUser(user);
         expect(preCondition.status()).toBe(201);
 
-        const response = await register.createUser(user);
+        const response = await auth.createUser(user);
         expect(response.status()).toBe(400);
 
         const body = await response.json();
         expect(body).toHaveProperty('message', 'Este e-mail já está em uso. Por favor, tente outro.');
     });
 
-    test('Não deve cadastrar com email inválido', async () => {
+    test('Deve retornar erro 400 quando o email é inválido', async () => {
         const user = {
             name: 'Danilo Costa',
             email: 'costa&gmail.com',
             password: 'Senha123',
         };
 
-        const response = await register.createUser(user);
+        const response = await auth.createUser(user);
         expect(response.status()).toBe(400);
 
         const body = await response.json();
         expect(body).toHaveProperty('message', 'O campo \'Email\' deve ser um email válido');
     });
 
-    test('Não deve cadastrar sem nome', async () => {
+    test('Deve retornar erro 400 quando o nome não é informado', async () => {
         const user = {
-            email: 'costa&gmail.com',
+            email: 'costa@gmail.com',
             password: 'Senha123',
         };
 
-        const response = await register.createUser(user);
+        const response = await auth.createUser(user);
         expect(response.status()).toBe(400);
 
         const body = await response.json();
         expect(body).toHaveProperty('message', 'O campo \'Name\' é obrigatório');
     });
 
-    test('Não deve cadastrar sem email', async () => {
+    test('Deve retornar erro 400 quando o email não é informado', async () => {
         const user = {
             name: 'Danilo Costa',
             password: 'Senha123',
         };
 
-        const response = await register.createUser(user);
+        const response = await auth.createUser(user);
         expect(response.status()).toBe(400);
 
         const body = await response.json();
         expect(body).toHaveProperty('message', 'O campo \'Email\' é obrigatório');
     });
 
-    test('Não deve cadastrar sem senha', async () => {
+    test('Deve retornar erro 400 quando a senha não é informada', async () => {
         const user = {
             name: 'Danilo Costa',
             email: 'costa@gmail.com',
         };
 
-        const response = await register.createUser(user);
+        const response = await auth.createUser(user);
         expect(response.status()).toBe(400);
 
         const body = await response.json();
